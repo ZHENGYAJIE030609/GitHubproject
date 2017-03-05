91eb9a59c3450bb3e6a1c432c113b8729146c4ea
ZHENGYAJIE030609 <2670370205@qq.com>
2017-03-05 13:51:36 +0800

    Initial commit

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/3/5 0005.
+ */
+public class TestUser {
+    @Test
+    public void test(){
+        System.out.println("hello git");
+    }
+}
